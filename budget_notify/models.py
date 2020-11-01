ff23115c3c1630debe78ca4edc1b1b0652eb401d
esl4m <me@esl4m.com>
2020-11-01 21:18:53 +0300

    Updates models , calculate average_spent

--- budget_notify/models.py
+++ budget_notify/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import datetime
 
 
 class Shop(models.Model):
@@ -18,9 +17,16 @@ class Budget(models.Model):
     Budgets Model
     """
     shop_id = models.ForeignKey('Shop', on_delete=models.CASCADE, db_column='a_shop_id')
-    month = models.DateTimeField(db_column='a_month')
+    month = models.CharField(max_length=5, db_column='a_month')
     budget_amount = models.DecimalField(max_digits=10, decimal_places=2, db_column='a_budget_amount')
     amount_spent = models.DecimalField(max_digits=10, decimal_places=2, db_column='a_amount_spent')
+
+    def _get_average_spent(self):
+        """
+            Returns the average spent.
+        """
+        return round((self.amount_spent * 100) / self.budget_amount , 2)
+    average_spent = property(_get_average_spent)
 
     class Meta:
         db_table = 't_budgets'  # override table name
